062931fdbb3db875b9aa3afd89582c0d35fd4ce2
Dennis S. Hennen <dennis@28studios.com>
2017-03-30 07:02:38 -0400

    wip

---
+++ src/SellerLabs/Mws/ServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+// From https://github.com/cviebrock/laravel5-package-template/blob/master/src/ServiceProvider.php
+
+namespace SellerLabs\Mws;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $configPath = __DIR__ . '/../config/mws.php';
+        $this->publishes([$configPath => config_path('mws.php')]);
+        $this->mergeConfigFrom($configPath, 'mws');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app->bind(MwsInterface::class, Mws::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [
+            MwsInterface::class,
+        ];
+    }
+}
